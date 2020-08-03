c6c960a447b971211351ce9ea9d5b3624de1557d
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-03 16:42:27 -0300

    fazendo a chamada do React dentro do código

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 const rootElement = document.getElementById('root');
-ReactDOM.render('Hello React!',
+ReactDOM.render(
+    <div>
+        <strong>Hello React!</strong>
+    </div>,
     rootElement
-    )
+)
