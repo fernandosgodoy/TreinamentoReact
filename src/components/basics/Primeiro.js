eadf56d461a6e42873032d518d77cf99c5d2254f
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-03 16:56:58 -0300

    criado um componente

---
+++ src/components/basics/Primeiro.js
@@ -0,0 +1,9 @@
+/**
+ * Crio uma função javascript e defino que ela será 
+ * exportada por padrão (export default)
+ */
+
+export default function primeiro() {
+    return 'Primeiro componente de exemplo'
+}
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/basics/Primeiro'
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(
     <div>
         <strong>Olá React!</strong>
+        <br/>
+        <Primeiro></Primeiro>
     </div>,
     rootElement
 )
