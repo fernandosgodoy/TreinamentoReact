6304c5726fb9e10ef07bb2a553baaface774c974
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-03 16:50:45 -0300

    importando o css para a aplicação

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
     <div>
-        <strong>Hello React!</strong>
+        <strong>Olá React!</strong>
     </div>,
     rootElement
 )
+
+// Outra forma de escrita
+/**
+ * const tag = <strong>Olá React!</strong>
+ * ...
+ * <div>
+ *      { tag }
+ * </div>
+ */
