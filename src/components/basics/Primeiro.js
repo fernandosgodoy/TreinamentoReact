5c3e1d0617c7061eeab028261fbc00bc83e14a18
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-03 17:04:20 -0300

    evoluindo este componente

--- src/components/basics/Primeiro.js
+++ src/components/basics/Primeiro.js
@@ -1,9 +1,35 @@
+import React from 'react'
+
 /**
  * Crio uma função javascript e defino que ela será 
  * exportada por padrão (export default)
  */
 
-export default function primeiro() {
-    return 'Primeiro componente de exemplo'
-}
+// export default function primeiro() {
+//     return <h2>Primeiro Componente</h2> //(1)
+// }
 
+/**
+ * (1)
+ * Se você precisar de uma estrutura aninhada
+ * de elementos html, é comum também ser escrito
+ * da seguinte maneira:
+ * 
+ * (
+ *  <div>
+ *      <h2>Primeiro Componente</h2>
+ *  </div>
+ * )
+ */
+
+export default function primeiro() {
+    const msg = "Lorem ipsum"
+    return ( 
+        <div>
+            <h2>Primeiro Componente</h2> 
+            <p>
+                { msg }
+            </p>
+        </div>
+    )
+}
