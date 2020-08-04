3a0362bb1efe2a8736889a40ceb62548546e1e48
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-04 15:26:19 -0300

    alterado o fragmento

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import Primeiro from './components/basics/Primeiro'
 import ChamaParametro from './components/basics/Parametros'
+import Fragmento from './components/basics/Fragmento'
 
 export default function() {
     return <div>
         <strong>Olá React!</strong>
         <br/>
+        <Fragmento titulo="Segundo Componente" 
+            subtitulo="Este é outro parâmetro" />
         <Primeiro></Primeiro>
         <ChamaParametro 
             titulo="Segundo Componente" 
