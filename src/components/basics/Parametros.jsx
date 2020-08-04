2d54c682e4228e721990b93686d329e5105e3cff
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-04 14:29:25 -0300

    trocada a nomenclatura para props

--- src/components/basics/Parametros.jsx
+++ src/components/basics/Parametros.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function RecebeParametros(params) {
+export default function RecebeParametros(props) {
     // console.log(params);
     return (
         <div>
-            <h2>{params.titulo}</h2>
-            <h3>{params.subtitulo}</h3>
+            <h2>{props.titulo}</h2>
+            <h3>{props.subtitulo}</h3>
         </div>
     )
 }
