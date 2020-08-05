c8e36c3511875acce9e2a1b68952e3cf8006afc7
Fernando Godoy <fernandogodoy.tec14@gmail.com>
2020-08-05 20:50:52 -0300

    desafio de randomizar um número aleatório com parâmetros

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 import Primeiro from './components/basics/Primeiro'
 import ChamaParametro from './components/basics/Parametros'
 import Fragmento from './components/basics/Fragmento'
+import Sortear from './components/SotearNumero'
 
 export default function() {
     return <div>
         <strong>Olá React!</strong>
         <br/>
+        <h1>Aulas</h1>
         <Fragmento titulo="Segundo Componente" 
             subtitulo="Este é outro parâmetro" />
         <Primeiro></Primeiro>
         <ChamaParametro 
             titulo="Segundo Componente" 
             subtitulo="Este é outro parâmetro" />
+        <hr/>
+        <Sortear minimo="1" maximo="60" />
     </div>
 }
---
+++ src/components/SotearNumero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Sortear(props) {
+    var randomNumber = getRandom(
+        props.minimo,
+        props.maximo
+    )
+
+    return (
+        <React.Fragment>
+            <h2>Sortear um número aleatório.</h2>
+            <span>
+                Resultado: 
+                <strong>
+                    {randomNumber}
+                </strong>
+            </span>
+        </React.Fragment>
+    )
+}
+
+function getRandom(min, max) {
+    return parseFloat(Math.random() * (max - min) + min).toFixed(0);
+}
