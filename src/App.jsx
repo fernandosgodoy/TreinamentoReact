c756146c14151d21c4859da4bada76c32c20722a
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-08-18 19:37:07 -0300

    aplicando o flex box para os cards da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,42 @@ import Card from './components/layout/Cards'
 import Primeiro from './components/basics/Primeiro'
 import ChamaParametro from './components/basics/Parametros'
 import Fragmento from './components/basics/Fragmento'
-import Sortear from './components/SotearNumero'
+import Sortear from './components/SotearNumero';
+import './App.css';
 
 export default function() {
-    return <div>
+    return <div className="App">
         <strong>Olá React!</strong>
         <br/>
         <h1>Aulas</h1>
 
-        <Card titulo="Desafio Aleatório">
-            <Sortear minimo="1" maximo="60" />
-            <Sortear minimo="1" maximo="60" />
-            <Sortear minimo="1" maximo="60" />
-            <Sortear minimo="1" maximo="60" />
-            <Sortear minimo="1" maximo="60" />
-            <Sortear minimo="1" maximo="60" />
-        </Card>
+        <div className="cards">
+            <Card titulo="Desafio Aleatório">
+                <Sortear minimo="1" maximo="60" />
+                <Sortear minimo="1" maximo="60" />
+                <Sortear minimo="1" maximo="60" />
+                <Sortear minimo="1" maximo="60" />
+                <Sortear minimo="1" maximo="60" />
+                <Sortear minimo="1" maximo="60" />
+            </Card>
 
-        <Card titulo="Fragmento">
-            <Fragmento titulo="Segundo Componente" 
-                subtitulo="Este é outro parâmetro" />
-        </Card>
+            <Card titulo="Fragmento">
+                <Fragmento titulo="Segundo Componente" 
+                    subtitulo="Este é outro parâmetro" />
+            </Card>
 
-        <Card titulo="Componente com Parâmetros">
-            <ChamaParametro 
-                titulo="Segundo Componente" 
-                subtitulo="Este é outro parâmetro" />
-        </Card>
+            <Card titulo="Componente com Parâmetros">
+                <ChamaParametro 
+                    titulo="Segundo Componente" 
+                    subtitulo="Este é outro parâmetro" />
+            </Card>
 
-        <Card titulo="Primeiro Componente">
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo="Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+
+        
         
     </div>
 }
